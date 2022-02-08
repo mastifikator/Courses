0f30eb3fd4a0475e7e80bd8e973535300919fa94
Семен Киреков <sakireko@mts.ru>
2022-02-08 10:47:17 +0300

    init

---
+++ src/main/java/com/mts/teta/DemoProjectApplication.java
@@ -0,0 +1,9 @@
+package com.mts.teta;
+
+public class DemoProjectApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+
+}
---
+++ src/test/java/com/mts/teta/DemoProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mts.teta;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DemoProjectApplicationTests {
+
+    @Test
+    void sanityTest() {
+        assertTrue(true);
+    }
+
+
+}
